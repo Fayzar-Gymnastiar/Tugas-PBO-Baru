9675746f638504c205c649ab32ed9109666c8eb6
Fayzar-Gymnastiar <fayzargymnastiar582@gmail.com>
2024-11-05 16:52:21 +0700

    Add files via upload

---
+++ slide 17.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fayzar Gymnastiar
+ * Nim     23215001
+ */
+public class NewClass {
+    public static void main(String[] args) {
+    String[] nama = new String[5];
+    nama[0] = "Thanos";
+    nama[1] = "Blackpanther";
+    nama[2] = "Thor";
+    nama[3] = "AntMan";
+    nama[4] = "Hulk";
+
+    for (int i = 0; i < 5; i++) {
+        /* Mengakses dan menampilkan array sesuai dengan variabel i yang dijadikan sebagai indeks */
+        System.out.println("Nama pada indeks " + i + ": " + nama[i]);
+    }
+}
+        
+    
+    
+}
+
+
+run:
+Nama pada indeks 0: Thanos
+Nama pada indeks 1: Blackpanther
+Nama pada indeks 2: Thor
+Nama pada indeks 3: AntMan
+Nama pada indeks 4: Hulk
+BUILD SUCCESSFUL (total time: 5 seconds)
